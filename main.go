a5277d09e52d4e374c361c24b597f524c490c654
Anudeep <you@example.com>
2022-10-19 17:50:39 +0530

    changed port

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/anudeep652/golang-bookstore-library-backend/router"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	app := book.Router()
 	fmt.Println(`app is listening on port $PORT`)
 
-	app.Listen(`:$PORT`)
+	app.Listen(":" + os.Getenv("PORT"))
 }

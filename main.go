1f502b06a4d1494621ca753c9f033d243ef0a2f2
Anudeep <you@example.com>
2022-10-19 17:45:42 +0530

    remove unused imports

--- main.go
+++ main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/anudeep652/golang-bookstore-library-backend/router"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	app := book.Router()
 	fmt.Println(`app is listening on port $PORT`)
